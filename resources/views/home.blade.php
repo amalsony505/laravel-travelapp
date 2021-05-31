36815fa00d4c2b6ab7aa87a0b65b4e3cf1a8d265
amalsony505 <mca505@rajagiri.edu>
2021-05-31 12:18:42 +0530

    travelapp

---
+++ resources/views/home.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body background="https://www.wishesmsg.com/wp-content/uploads/happy-vacation.jpg">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/offers">Offers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/blog">blog</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/signup">Sign Up</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+      
+
+    <div class="container">
+    <br><br>
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                
+                
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                <table class="table  table-borderless">
+                <tr>
+                    <td>
+                    <center><label><h2 style="color:black">LOGIN</h2></label></center><br> 
+                    <label><h3 style="color:black">Username:</h3></label>
+                    <input type="text" class="form-control">
+                    <br>
+                    <label><h3 style="color:black">Password:</h3></label>
+                    <input type="password" class="form-control">
+                    <br>
+                    <button class="btn btn-success" type="submit">LOGIN</button>
+                    </td>
+                </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-md-4 col-xl-4 col-xxl-4">
+                <br><br><br>
+                
+                  
+            </div>
+        </div>
+    </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>   
+</body>
+</html>
